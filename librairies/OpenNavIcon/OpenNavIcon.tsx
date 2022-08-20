7f91d8ae602ed6bf0dffcbd722a3b59bd8981c0b
felix fedronic <fedronicfelix@gmail.com>
2022-08-20 17:59:40 +0200

    navbutton done

--- librairies/OpenNavIcon/OpenNavIcon.tsx
+++ librairies/OpenNavIcon/OpenNavIcon.tsx
@@ -8,20 +8,20 @@ function OpenNavButton(props: ToggleHandler) {
   const { toggleHandler } = props;
 
   return (
-    <button className="z-10" onClick={toggleHandler}>
+    <div className="z-10" onClick={toggleHandler}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5"
         viewBox="0 0 20 20"
-        fill="#64ffda"
+        fill="#64ffde"
       >
         <path
           fillRule="evenodd"
-          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
           clipRule="evenodd"
         />
       </svg>
-    </button>
+    </div>
   );
 }
 
